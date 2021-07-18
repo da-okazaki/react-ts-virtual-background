0559a0b27f0502f29d9eb7ad5dc12d237fa68228
da-okazaki <da-okazaki@outlook.com>
2021-07-18 19:33:00 +0900

    [modify] Fixed background removal process for canvas.

--- src/components/vg-camera.tsx
+++ src/components/vg-camera.tsx
@@ -9,9 +9,9 @@ type EffectType = "none" | "bokeh" | "image";
 const VgCamera: FC = () => {
   const classes = useStyles();
   const [model, setModel]: any = useState();
-  const [mask, setMask]: any = useState();
-  const [isVgMode, setIsVgMode]: any = useState(true)
+  const [isVgMode, setIsVgMode]: any = useState(false)
   const [callBackId, setCallBackId]: any = useState()
+  const [effectTypeState, setEffectTypeState] = useState<EffectType>("none");
 
   useEffect(() => {
     const params: any = {
@@ -26,48 +26,75 @@ const VgCamera: FC = () => {
     });
   }, []);
 
-  useEffect(() => {
-    const webcam: any = document.getElementById("webcam");
-    const canvas: any = document.getElementById("canvas");
-    const opacity = 0.7;
+  /**
+   * compositeFrame
+   */
+  const compositeFrame = async (video: any, canvas: any, backgroundDarkeningMask: any) => {
+    if (!backgroundDarkeningMask) return;
+    // grab canvas holding the bg image
+    var ctx = canvas.getContext('2d');
+    // composite the segmentation mask on top
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.putImageData(backgroundDarkeningMask, 0, 0);
+    // composite the video frame
+    ctx.globalCompositeOperation = 'source-in';
+    ctx.drawImage(video, 0, 0, 640, 480);
+  }
 
-    bodyPix.drawMask(canvas, webcam, mask, opacity, 0, false);
-  }, [mask]);
+
+  /**
+   * removeBg
+   */
+  const removeBg = async () => {
+    const video: any = document.getElementById("webcam");
+    const canvas: any = document.getElementById("canvas");
+
+    // ? Segmentation occurs here, taking video frames as the input
+    const segmentation = await model.segmentPerson(video, {
+        flipHorizontal: false,
+        internalResolution: 'medium',
+        segmentationThreshold: 0.5
+    });
+
+    const foregroundColor = { r: 0, g: 0, b: 0, a: 255 };
+    const backgroundColor = { r: 0, g: 0, b: 0, a: 0 };
+    const backgroundDarkeningMask = bodyPix.toMask(segmentation, foregroundColor, backgroundColor, false);
+    compositeFrame(video, canvas, backgroundDarkeningMask);
+    setCallBackId(requestAnimationFrame(removeBg))
+  }
 
   const startRenderFrame = () => {
     setIsVgMode(true)
-    const drawMask = async () => {
-      const webcam: any = document.getElementById("webcam");
-      const canvas: any = document.getElementById("canvas");
-      const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
-      const backgroundColor = { r: 127, g: 127, b: 127, a: 255 };
-
-      const seg = await model.segmentPerson(webcam)
-
-      const mask = bodyPix.toMask(seg, foregroundColor, backgroundColor, true)
-      const opacity = 0.7;
-
-      bodyPix.drawMask(canvas, webcam, mask, opacity, 0, false);
-      setCallBackId(requestAnimationFrame(drawMask))
-    }
-    drawMask()
+    removeBg()
   };
 
   const stopRenderFrame = useCallback(() => {
+    setIsVgMode(false)
     cancelAnimationFrame(callBackId)
   }, [callBackId]);
 
   return (
-    <>
-      <Box>
+    <div style={{ width: "100%", height: "100%" }}>
+      <Box style={{ position: "absolute", top: 100, zIndex: 500 }}>
         <Button onClick={startRenderFrame}>推定</Button>
         <Button onClick={stopRenderFrame}>停止</Button>
       </Box>
-      <Box>
-        <Webcam id="webcam" style={{ transform: "scaleX(-1)"}} width={640} height={480} />
-        <canvas id="canvas" style={{ transform: "scaleX(-1)"}} width={640} height={480} />
-      </Box>
-    </>
+
+      <div className={classes.wrapperStyle}>
+        <Webcam
+          id="webcam"
+          className={classes.cameraStyle}
+          width={640}
+          height={480}
+        />
+        <canvas
+          id="canvas"
+          className={isVgMode ? classes.canvasStyle : classes.disableStyle}
+          width={640}
+          height={480}
+        />
+      </div>
+    </div>
   );
 };
 export default VgCamera;
@@ -104,6 +131,7 @@ const useStyles = makeStyles(() =>
       position: "absolute",
       zIndex: 20,
       transform: "scaleX(-1)",
+      backgroundImage: "url(../assets/vg_image_001.jpeg)"
     },
 
     disableStyle: {
